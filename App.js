e468ed4421bc28d057d6ca08ebac9cd0f23c50d7
Mounikaveerla29 <mounikaveerla111@gmail.com>
2026-05-21 18:11:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import PasswordManager from './components/PasswordManager'
+import './App.css'
+
+const App = () => (
+  <div className="app">
+    <PasswordManager />
+  </div>
+)
+
+export default App
